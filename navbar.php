00135aaf2b8b4e270073eb84a2a0887a939f4ba7
ToastnotAI <ryanchristie03@gmail.com>
2025-03-10 13:27:09 +0000

    add dropdown to navbar

--- navbar.php
+++ navbar.php
@@ -1,8 +1,13 @@
-<div class = "navBar">
-	<ul>
-		<li><a href = "index.php">Home</a></li>
-		<li><a href = "aboutme.php">About Me</a></li>
-		<li><a href = 'layout1.php'>Layout 1</a></li>
-		<li><a href = 'layout2.php'>Layout 2</a></li>
-	</ul>
-</div>
+<div class="navBar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="aboutme.php">About Me</a></li>
+            <li class="dropdown"><a href='#'>Layouts</a>
+                <ul class="dropdown-content">
+                    <li><a href='layout1.php'>Layout 1</a></li>
+                    <li><a href='layout2.php'>Layout 2</a></li>
+                </ul>
+            </li>
+            <li><a href='aboutCookies.php'>About Cookies</a></li>
+        </ul>
+    </div>
